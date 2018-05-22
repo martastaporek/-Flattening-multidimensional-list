54b704813ae89ec42eb30378051f9f845acf77e6
martastaporek <staporekmarta1@gmail.com>
2018-05-22 18:09:17 +0200

    Implement add objects

--- src/main/java/ListFlatter.java
+++ src/main/java/ListFlatter.java
@@ -1,2 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListFlatter {
-}
+
+    List flatted = new ArrayList();
+
+
+    public List getFlatted() {
+        return flatted;
+    }
+
+    public void flaten(List nestedList) {
+
+        for (Object elem : nestedList) {
+            if (elem instanceof Iterable) {
+                Iterable castedElem = (Iterable) elem;
+                flatenList(castedElem);
+            } else if (elem instanceof Object []) {
+                Object [] castedElem = (Object[]) elem;
+                flatenList(castedElem);
+            } else {
+                this.flatted.add(elem);
+            }
+        }
+    }
+
+
+
+
+    private void flatenList(Iterable nestedElem) {
+
+        for (Object elem : nestedElem) {
+            if (elem instanceof Iterable) {
+                Iterable castedElem = (Iterable) elem;
+                flatenList(castedElem);
+            } else if (elem instanceof Object []) {
+            Object [] castedElem = (Object[]) elem;
+            flatenList(castedElem);
+        } else {
+            this.flatted.add(elem);
+        }
+        }
+
+        }
+
+    private void flatenList(Object[] nestedElem) {
+        for (Object elem : nestedElem) {
+            if (elem instanceof Iterable) {
+                Iterable castedElem = (Iterable) elem;
+                flatenList(castedElem);
+            } else if (elem instanceof Object []) {
+            Object [] castedElem = (Object[]) elem;
+            flatenList(castedElem);
+        } else {
+            this.flatted.add(elem);
+        }
+
+            }
+        }
+    }
